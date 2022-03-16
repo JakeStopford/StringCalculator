05af2b90f16ade4ee147c3d0e4fc8a0d951c89ac
Jake Stopford <Jake_Stopford@hotmail.com>
2022-03-16 16:41:22 +0000

    Added unit tests with solution

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -1,7 +1,46 @@
-﻿namespace StringCalculator
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringCalculator
 {
-    class StringCalculator
+    public class StringCalculator
     {
-        public int Add()
+        public int Add(string numbersCollection)
+        {
+            List<int> negativeNumbers = new List<int>();
+            var delimiters = new List<char> {',', '\n'};
+
+            if (numbersCollection.StartsWith("//"))
+            {
+                delimiters.Add(numbersCollection[2]);
+            }
+
+            var charArray = numbersCollection.Split(delimiters.ToArray());
+
+            int sum = 0;
+            foreach (string strNumber in charArray)
+            {
+                int number;
+                if (int.TryParse(strNumber, out number) && number <= 1000)
+                {
+                    if (number < 0)
+                    {
+                        negativeNumbers.Add(number);
+                    }
+                    else
+                    { 
+                        sum += number;
+                    }
+                }
+            }
+
+            if (negativeNumbers.Count > 0)
+            {
+                throw new Exception("Negative numbers!");
+                // print out all negatives
+            }
+
+            return sum;
+        }
     }
 }
--- StringCalculator/StringCalculatorTests.cs
+++ StringCalculator/StringCalculatorTests.cs
@@ -7,29 +7,72 @@ namespace StringCalculator
         [SetUp]
         public void Setup()
         {
+
+        }
+
+        [Test]
+        public void EmptyString_Returns0()
+        {
+            // Arrange
             StringCalculator stringCalculator = new StringCalculator();
+
+            // Act
+            var result = stringCalculator.Add("");
+
+            // Assert
+            Assert.AreEqual(0, result);
         }
 
         [Test]
-        public void Test1()
+        public void AddNumbers_WithCommaDelimiter()
         {
             // Arrange
+            StringCalculator stringCalculator = new StringCalculator();
 
             // Act
+            var result = stringCalculator.Add("1,2,3");
 
             // Assert
-            Assert.Pass();
+            Assert.AreEqual(6, result);
         }
 
         [Test]
-        public void Test2()
+        public void AddNumbers_WithNewLinesAndDelimiter()
         {
             // Arrange
+            StringCalculator stringCalculator = new StringCalculator();
 
             // Act
+            var result = stringCalculator.Add("1\n2,3");
 
             // Assert
-            Assert.Pass();
+            Assert.AreEqual(6, result);
+        }
+
+        [Test]
+        public void AddNumbers_WithNewLinesAndCustomDelimiter()
+        {
+            // Arrange
+            StringCalculator stringCalculator = new StringCalculator();
+
+            // Act
+            var result = stringCalculator.Add("//;\n1;2");
+
+            // Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void AddNumbers_WithNewLinesAndMultipleDelimiters()
+        {
+            // Arrange
+            StringCalculator stringCalculator = new StringCalculator();
+
+            // Act
+            var result = stringCalculator.Add("//;\n1;2");
+
+            // Assert
+            Assert.AreEqual(3, result);
         }
     }
 }
