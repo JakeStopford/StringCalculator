2e9ad108f2a9f316b26ece22a2a618b2b04aa39f
Jake Stopford <Jake_Stopford@hotmail.com>
2022-03-18 19:23:58 +0000

    added more test cases

--- StringCalculator/StringCalculatorTests.cs
+++ StringCalculator/StringCalculatorTests.cs
@@ -23,55 +23,80 @@ namespace StringCalculator
         }
 
         [Test]
-        public void AddNumbers_WithCommaDelimiter()
+        [TestCase("1,2,3", 6)]
+        [TestCase("5,18,37", 60)]
+        [TestCase("20,20,100", 140)]
+        public void AddNumbers_WithCommaDelimiter(string str, int expected)
         {// Act
-            var result = StringCalculator.Add("1,2,3");
+            var result = StringCalculator.Add(str);
 
             // Assert
-            Assert.AreEqual(6, result);
+            Assert.AreEqual(expected, result);
         }
 
         [Test]
-        public void AddNumbers_WithNewLinesAndDelimiter()
+        [TestCase("1\n2,3", 6)]
+        [TestCase("1\n4,6", 11)]
+        [TestCase("5\n5,5", 15)]
+        public void AddNumbers_WithNewLinesAndDelimiter(string str, int expected)
         {
             // Act
-            var result = StringCalculator.Add("1\n2,3");
+            var result = StringCalculator.Add(str);
 
             // Assert
-            Assert.AreEqual(6, result);
+            Assert.AreEqual(expected, result);
         }
 
         [Test]
-        public void AddNumbers_WithNewLinesAndCustomDelimiter()
+        [TestCase("//;\n1;2", 3)]
+        [TestCase("//^\n1^2^3", 6)]
+        [TestCase("//!\n1!2!3!4!5", 15)]
+        public void AddNumbers_WithNewLinesAndCustomDelimiter(string str, int expected)
         {
-            // Arrange
-            StringCalculator stringCalculator = new StringCalculator();
-
             // Act
-            var result = StringCalculator.Add("//;\n1;2;1001");
+            var result = StringCalculator.Add(str);
 
             // Assert
-            Assert.AreEqual(3, result);
+            Assert.AreEqual(expected, result);
         }
 
         [Test]
-        public void AddNumbers_WithNewLinesAndAnyDelimiterLength()
+        [TestCase("//;\n1;2;1001", 3)]
+        [TestCase("//;\n10;20;99999999", 30)]
+        [TestCase("//;\n1;2;1000;1001", 1003)]
+        public void AddNumbers_WithNumberGreaterThan1000_ReturnsCorrectSum(string str, int expected)
         {
             // Act
-            var result = StringCalculator.Add("//[***]\n1***2***3");
+            var result = StringCalculator.Add(str);
 
             // Assert
-            Assert.AreEqual(6, result);
+            Assert.AreEqual(expected, result);
         }
 
         [Test]
-        public void AddNumbers_WithNewLinesAndMultipleDelimiters()
+        [TestCase("//[***]\n1***2***3", 6)]
+        [TestCase("//[%*£!!!]\n1%*£!!!1001%*£!!!39%*£!!!11", 51)]
+        [TestCase("//[%*£]\n33%*£33%*£33",99)]
+        public void AddNumbers_WithNewLinesAndAnyDelimiterLength(string str, int expected)
         {
             // Act
-            var result = StringCalculator.Add("//[*][%]\n1*2%3");
+            var result = StringCalculator.Add(str);
 
             // Assert
-            Assert.AreEqual(6, result);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        [TestCase("//[*][%]\n1*2%3", 6)]
+        [TestCase("//[%*£!!!][*][%]\n1%*£!!!2%19", 22)]
+        [TestCase("//[%*£!!!][*][%]\n1001%*£!!!2%50", 52)]
+        public void AddNumbers_WithNewLinesAndMultipleDelimiters(string str, int expected)
+        {
+            // Act
+            var result = StringCalculator.Add(str);
+
+            // Assert
+            Assert.AreEqual(expected, result);
         }
     }
 }
